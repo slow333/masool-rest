5f9651a76d045e88ce563847345b252d68b7d92a
limp bizkit <slow33@gmail.com>
2025-03-08 12:08:54 +0900

    first commit

---
+++ src/main/java/ma/sool/SoolApplication.java
@@ -0,0 +1,13 @@
+package ma.sool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/sool/SoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.sool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
