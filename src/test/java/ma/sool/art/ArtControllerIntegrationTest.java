98e99616ffdcbf91e0034fd71007b9a74cea7650
limp bizkit <slow33@gmail.com>
2025-03-22 10:07:44 +0900

    security 관련 ãtest change(jwt authentication code added)

---
+++ src/test/java/ma/sool/art/ArtControllerIntegrationTest.java
@@ -0,0 +1,92 @@
+package ma.sool.art;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ma.sool.system.StatusCode;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.lang.reflect.Method;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("통합 시험, API endpoint")
+@Tag("통합시험")
+public class ArtControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Value("${api.base-url}")
+    String url;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    String token;
+
+    @BeforeEach
+    void setup() throws Exception {
+        ResultActions result = mockMvc.perform(post(url + "/users/login")
+                .with(httpBasic("kim", "123456")));
+        MvcResult mvcResult = result.andDo(print()).andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        JSONObject jsonObject = new JSONObject(contentAsString);
+        this.token = "Bearer " + jsonObject.getJSONObject("data").getString("token");
+
+    }
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
+    @DisplayName("arts find all 통합시험")
+    void 아트모두찾기성공() throws Exception {
+        mockMvc.perform(get(url + "/arts").accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.flag").value(true))
+                .andExpect(jsonPath("$.code").value(StatusCode.SUCCESS))
+                .andExpect(jsonPath("$.message").value("Find All Success"))
+                .andExpect(jsonPath("$.data", Matchers.hasSize(6)));
+    }
+    @Test
+    @DisplayName("포스트에 대한 Add arts 시험")
+    void 마술추가하기성공시험() throws Exception {
+        // given
+        Art a = new Art();
+        a.setName("마술기술");
+        a.setDescription("추가한 마술 기술");
+        a.setImgUrl("이미지");
+
+        String json = objectMapper.writeValueAsString(a);
+        // when and then
+        mockMvc.perform(post(url+"/arts")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON).content(json)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.flag").value(true))
+                .andExpect(jsonPath("$.code").value(StatusCode.SUCCESS))
+                .andExpect(jsonPath("$.message").value("Add Success"))
+                .andExpect(jsonPath("$.data.id").isNotEmpty())
+                .andExpect(jsonPath("$.data.name").value("마술기술"));
+        mockMvc.perform(get(url+"/arts").accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.flag").value(true))
+                .andExpect(jsonPath("$.code").value(StatusCode.SUCCESS))
+                .andExpect(jsonPath("$.message").value("Find All Success"))
+                .andExpect(jsonPath("$.data", Matchers.hasSize(7)));
+    }
+}
